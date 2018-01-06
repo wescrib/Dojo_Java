81e4806ab8b14ba7f0467ba9600c9ddc3f977c3c
wescrib <scribner.william@gmail.com>
2018-01-06 17:07:09 -0500

    committing the Life and HelloWorld assignments

---
+++ First_Assignment/Life.java
@@ -0,0 +1,13 @@
+public class Life {
+	public static void main(String[] args) {
+		String firstName = "William";
+		String lastName = "Scribner";
+		int age = 26;
+		String hometown = "San Francisco, CA";
+
+		System.out.println("My name is " + firstName + " " + lastName);
+		System.out.println("I am " + age + "years old.");
+		System.out.println("My hometown is " + hometown);
+
+	}
+}
