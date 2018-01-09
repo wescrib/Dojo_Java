cedb9fd472f45915467268fa7cf20273a95e94cf
wescrib <scribner.william@gmail.com>
2018-01-08 20:22:14 -0500

    committing basic algorithms

---
+++ Basics/BasicJava.java
@@ -0,0 +1,9 @@
+public class BasicJava {
+    public static void main(String [] args) {
+        ArrayList<Integer> two55Arr = new ArrayList<Integer>();
+        for(int i=1; i <=255; i++) {
+            two55Arr.add("i");
+        }
+        System.out.println(two55Arr);
+    }
+}
