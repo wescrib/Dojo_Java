dc18f4c54f5ee46648713508637780d655f4faad
wescrib <scribner.william@gmail.com>
2018-01-15 18:13:10 -0500

    commiting hello human

---
+++ HelloHuman/src/main/java/com/project/HelloHuman/controllers/Router.java
@@ -0,0 +1,43 @@
+package com.project.HelloHuman.controllers;
+
+import java.security.Principal;
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping("/*") // Wildcard all routes.
+public class Router{
+	public Router(){
+
+	}
+
+	@RequestMapping("/")
+	public String index(@RequestParam(value="name") String search, HttpSession session){
+		if(session.getAttribute("name")==null) {
+			session.setAttribute("name", "Human");
+		} else {
+			session.setAttribute("name", search);
+		}
+		return "index";
+	}
+
+	@RequestMapping("")
+	public String redirect(HttpServletRequest req){		
+		String url = req.getRequestURI().toString();
+		return "redirect:/index";
+	}		
+}
